9dac2e4c93ddc74aa5f75c06967b4463454ff1ff
lei10003 <ligea.alex@gmail.com>
2023-08-20 00:29:54 -0400

    changes

--- Creative Coding/index.js
+++ Creative Coding/index.js
@@ -1,1 +1,22 @@
-let x = 5;
+function saySomething (param) {
+    console.log(param);
+}
+
+saySomething('hey there');
+
+function add(a,b) {
+   return  a+ b;
+}
+
+let result = add(5,7);
+console.log(result);
+
+let multiply = (a,b) =>{
+return a*b;
+}
+console.log(multiply(5,7));
+
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+context.fillRect(100, 100, 400, 400);
